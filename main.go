0a55db061dd420ddad153f1d5ecb49bc1fc93715
Roelof Ruis <roelof.ruis@gmail.com>
2022-02-02 23:58:31 +0100

    better help text

--- main.go
+++ main.go
@@ -18,16 +18,16 @@ func main() {
 		timesheetRepository: TimesheetRepository{fileIO: fileIO},
 	}
 
-	router := router{actions: make(map[string]handler)}
-	router.register("start", app.handleStart)
-	router.register("stop", app.handleStop)
-	router.register("tag", app.handleTag)
-	router.register("show", app.handleShow)
-	router.register("purge", app.handlePurge)
+	router := newRouter()
+	router.register("start", "start a new block", app.handleStart)
+	router.register("stop", "stop the active block", app.handleStop)
+	router.register("tag", "change active tags", app.handleTag)
+	router.register("show", "show timesheet", app.handleShow)
+	router.register("purge", "purge timesheet", app.handlePurge)
 
 	err, msg := router.handle(os.Args[1:])
 	if err != nil {
-		fmt.Printf("error: %s", err.Error())
+		fmt.Printf("error: %s\n", err.Error())
 	}
 	fmt.Printf("%s\n", msg)
 }
--- router.go
+++ router.go
@@ -1,26 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type handler = func(args []string) (error, string)
 
 type router struct {
-	actions map[string]handler
+	actions      map[string]handler
+	descriptions map[string]string
 }
 
-func (r *router) register(action string, handler handler) {
+func newRouter() *router {
+	return &router{
+		actions:      make(map[string]handler),
+		descriptions: make(map[string]string),
+	}
+}
+
+func (r *router) register(action string, description string, handler handler) {
 	r.actions[action] = handler
+	r.descriptions[action] = description
 }
 
 func (r *router) handle(args []string) (error, string) {
 	if len(args) == 0 {
-		return fmt.Errorf("no command given"), ""
+		return fmt.Errorf("no command given"), r.helpMessage()
 	}
 
 	h, has := r.actions[args[0]]
 	if !has {
-		return fmt.Errorf("unknown command '%s'", args[0]), ""
+		return fmt.Errorf("unknown command '%s'", args[0]), r.helpMessage()
 	}
 
 	return h(args[1:])
+}
+
+func (r *router) helpMessage() string {
+	lines := []string{fmt.Sprintf("available actions:")}
+	for action, description := range r.descriptions {
+		lines = append(lines, fmt.Sprintf("  - %s\n  %s", action, description))
+	}
+	return strings.Join(lines, "\n")
 }
